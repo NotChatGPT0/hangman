3e420cec1da12f2d7e27b6292239c0a4a457deb5
notchatgpt <160557384+notchatgptbiz@users.noreply.github.com>
2026-03-06 22:19:57 -0500

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -1,4 +1,8 @@
 userinput = input("Guess a letter")
 print(userinput)
 listofwords = [tree, car, nickel, hangman]
-if
+selectedword = random.choice(listofwords)
+if userinput in selectedword:
+  print("You guessed a letter correctly!")
+else:
+  print(Yyou've guessed a letter incorrectly!")
